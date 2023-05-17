7e4ad2e4d9d967e1fd566f69765b2a26426513c3
Vikas <vikas807@gmail.com>
2023-05-17 21:04:51 +0530

    added bet function and line function

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
+
 
 def deposit():
     while True:
@@ -11,9 +15,53 @@ def deposit():
         else:
             print("Please enter a number.")
 
-        return amount
+    return amount
+
+def get_number_of_lines():
+    while True:
+        lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Enter valid number of lines")
+        else:
+            print("Please enter a number.")
+
+    return lines
+
+def get_bet():
+    while True:
+        amount = input("What would you like to bet on each line? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f"Amount must be between {MIN_BET} - {MAX_BET}")
+        else:
+            print("Please enter a number.")
+
+    return amount
+
+
     
 
-deposit()
+def main():
+    balance = deposit()
+    lines =  get_number_of_lines()
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+
+        if total_bet >= balance:
+            print(f"You do not have enough to bet that amount, your current balance is: ${balance}")
+        else:
+            break
+    print(f"You are beting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}")
+
+
+main()
 
 
